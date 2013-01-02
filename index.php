e2a4f3727cbd0fc7917fc86696b662f1cef16911
Shumi2010 <sneveld@rambler.ru>
2013-01-02 09:40:37 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,127 @@
+<html>
+<head>
+
+        <title>Сайт JuniorSport - мировой юниорский спорт  </title>
+	<META Name="keywords" Content=" ">
+	<META Name="description" Content="Сайт JuniorSport - мировой юниорский спорт ">
+
+<?php include 'txt/css.txt';?>
+
+</head>
+
+<?php include 'txt/color.txt';?>
+
+<table border="0" cellpadding="10" cellspacing="0" width="100%" background="">
+   <tr>
+       <td width="15%" valign="center">
+           <?php
+           include 'txt/bunner.txt';
+           ?> 
+       </td>
+       
+       <td width="65%" valign="center">
+           <?php
+           include 'txt/head.txt';
+           ?> 
+       </td>
+    
+       <td width="20%" valign="center">
+           <?php
+           include 'txt/search.txt';
+           ?>
+       </td>  
+   </tr>
+  
+   <tr>
+       <td width="15%" valign="top">
+          <?php
+            include 'txt/menu.txt';
+          ?> 
+       </td>
+       
+       <td width="65%" valign="top">
+	
+	<table border="0" cellpadding="10" cellspacing="0" width="100%" background="">
+        <tr>
+        <td valign="center">
+	<?php	
+	   include 'txt/up_menu.txt';	
+         ?>
+	</td>
+	</tr>
+
+        <tr>
+        <td valign="top">
+<center><h2>JuniorSport - мировой юниорский спорт</h2></center>
+	
+	</td>
+	</tr>
+	</table>
+       </td>
+    
+       <td width="20%" valign="top">
+          <?php
+          include 'txt/reclama.txt';
+          ?>
+       </td>   
+   </tr> 	
+
+</table>
+
+<table border="0" cellpadding="10" cellspacing="0" width="100%" background="">
+    <tr>
+    <td valign="center">
+     <?php
+      include 'txt/bottom_menu.txt';
+      ?>
+    </td>
+    </tr>	
+</table>
+
+<table border="0" cellpadding="10" cellspacing="0" width="100%" background="">
+    <tr>
+    <td valign="center">
+      <?php
+      include 'txt/tizer.txt';
+      ?>
+    </td>
+    </tr>	
+</table>
+
+<table border="0" cellpadding="10" cellspacing="0" width="100%" background="">
+   <tr>
+       <td width="30%" valign="center">
+          <?php
+           include 'txt/ref1.php';
+           ?> 
+       </td>
+
+       <td widht="40%" valign="center">  
+          <?php 
+           include 'txt/contact.txt';
+           ?>
+       </td> 
+
+       <td width="30%" valign="center">
+          <?php 
+           include 'txt/ref2.php';
+          ?>
+       </td>
+
+   </tr>
+</table>
+
+<table border="0" cellpadding="10" cellspacing="0" width="100%" background="">
+    <tr>
+    <td valign="center">
+      <?php
+      include 'txt/counter.php';
+      ?>
+    </td>
+    </tr>	
+</table>
+
+</body>
+
+</html>
+
